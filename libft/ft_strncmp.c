2274f5e0ca3e64be8b7c1a1503163722409ec539
Henrique Lelli <henriquelelli@hotmail.com>
2022-06-18 19:26:12 +0200

    adição do ft_strncmp corrigido

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,12 +6,11 @@
 /*   By: hcesar-l <hcesar-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 21:32:45 by hcesar-l          #+#    #+#             */
-/*   Updated: 2022/06/02 22:17:17 by hcesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/18 17:21:17 by hcesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
