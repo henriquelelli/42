42f03642ab7e3703b5670607667c9e49fc4e5fae
Henrique Lelli <henriquelelli@hotmail.com>
2022-06-18 19:27:32 +0200

    atualização na libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hcesar-l <hcesar-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 21:06:31 by hcesar-l          #+#    #+#             */
-/*   Updated: 2022/06/02 22:09:34 by hcesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/18 19:13:01 by hcesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
+int		ft_atoi(const char *nptr);
 int		ft_isalnum(int c);
+int		ft_isalpha(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
+void	ft_putchar_fd(char c, int fd);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 #endif
