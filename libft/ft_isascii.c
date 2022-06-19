76941b20c015435326900b59460141b0b10e7f44
Henrique Lelli <henriquelelli@hotmail.com>
2022-06-19 20:11:14 +0200

    melhorando os códigos

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,13 +6,11 @@
 /*   By: hcesar-l <hcesar-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 21:31:59 by hcesar-l          #+#    #+#             */
-/*   Updated: 2022/05/31 21:49:50 by hcesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/19 20:10:02 by hcesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isascii(int c)
 {
-	if (c >= 0 && c < 128)
-		return (1);
-	return (0);
+	return (c >= 0 && c < 128)
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hcesar-l <hcesar-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 21:06:31 by hcesar-l          #+#    #+#             */
-/*   Updated: 2022/06/19 00:11:20 by hcesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/19 20:08:41 by hcesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+void	ft_memset(void *s, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
