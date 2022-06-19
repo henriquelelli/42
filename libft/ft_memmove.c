267dc3eead6e261299949a2077f92f3542943209
Henrique Lelli <henriquelelli@hotmail.com>
2022-06-19 21:04:43 +0200

    melhoria no código

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,25 +6,25 @@
 /*   By: hcesar-l <hcesar-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 20:37:43 by hcesar-l          #+#    #+#             */
-/*   Updated: 2022/06/19 20:43:57 by hcesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/19 20:54:13 by hcesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *s1, const void *s2, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*s1_aux;
 	unsigned char	*s2_aux;
 	
-	s1_aux = (unsigned char *)s1;
-	s2_aux = (unsigned char *)s2;
-	if (s2 < s1)
+	s1_aux = (unsigned char *)dest;
+	s2_aux = (unsigned char *)src;
+	if (src < dest)
 	{
 		while (n--)
 			s1_aux[n] = s2_aux[n];
 	}
 	else
-		ft_memcpy(s1, s2, n);
-	return (s1);
+		ft_memcpy(dest, src, n);
+	return (dest);
 }
