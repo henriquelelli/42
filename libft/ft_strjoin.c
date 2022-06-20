4262f6ff67c37d39acff6e52091bf4629d031ef1
Henrique Lelli <henriquelelli@hotmail.com>
2022-06-20 21:21:33 +0200

    correções de norminette

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hcesar-l <hcesar-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 21:01:40 by hcesar-l          #+#    #+#             */
-/*   Updated: 2022/06/20 21:11:28 by hcesar-l         ###   ########.fr       */
+/*   Updated: 2022/06/20 21:17:44 by hcesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	str_ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
+	str_ptr = (char *)malloc(
+			(ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	str = str_ptr;
+	if (str_ptr == NULL)
+		return (NULL);
 	while (*s1)
 		*str_ptr++ = *s1++;
 	while (*s2)
 		*str_ptr++ = *s2++;
 	*str_ptr = '\0';
-	reutn (str);
+	return (str);
 }
